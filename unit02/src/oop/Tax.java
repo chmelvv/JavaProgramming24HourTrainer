45f6d88e80944d22f842514d396a427bdeb9a909
Viktor Chmel <Viktor.Chmel@travisperkins.co.uk>
2015-08-05 02:04:59 +0300

    [add] Tax project

--- unit02/src/oop/Tax.java
+++ unit02/src/oop/Tax.java
@@ -1,5 +1,12 @@
 package oop;
 
 public class Tax {
-
+	double grossIncome;
+	String state;
+	int dependents;
+	
+	public double calcTax(){
+		if (grossIncome > 50000) return grossIncome*0.08;
+		else return grossIncome*0.06;
+	}
 }
--- unit02/src/oop/TestTax.java
+++ unit02/src/oop/TestTax.java
@@ -3,8 +3,13 @@ package oop;
 public class TestTax {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Tax t = new Tax();
+		t.dependents = 2;
+		t.grossIncome = 1000.0;
+		t.state = "NJ";
+		
+		System.out.println(t.calcTax());
+		
 	}
 
 }
