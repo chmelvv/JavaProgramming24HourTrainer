39249aaf9f46f5b9b56bd3224fc44187e1e3fd94
Viktor Chmel <Viktor.Chmel@travisperkins.co.uk>
2015-08-05 01:32:25 +0300

    [add] Car task

--- unit02/src/oop/Car.java
+++ unit02/src/oop/Car.java
@@ -1,5 +1,14 @@
 package oop;
 
 public class Car {
-
+	String color;
+	int numberOfDors;
+	
+	void startEngine(){
+		
+	}
+	
+	void stopEngine(){
+		int tempCounter=0;
+	}
 }
--- unit02/src/oop/TestCar.java
+++ unit02/src/oop/TestCar.java
@@ -3,7 +3,13 @@ package oop;
 public class TestCar {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Car car1 = new Car();
+		Car car2 = new Car();
+		
+		car1.color = "blue";
+		car2.color = "red";
+		
+		System.out.println("Cars have been painted car1: " + car1.color + " car2: " + car2.color);
 
 	}
 
