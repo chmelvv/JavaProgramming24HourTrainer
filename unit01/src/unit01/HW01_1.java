9e7ea5708735302dd79169b98f2fb851e0d94f97
Viktor Chmel <Viktor.Chmel@travisperkins.co.uk>
2015-08-04 13:10:30 +0300

    [done]

---
+++ unit01/src/unit01/HW01_1.java
@@ -0,0 +1,17 @@
+package unit01;
+
+public class HW01_1 {
+
+	public static void main(String[] args) {
+/*		Alex	Johnson	
+		23	Main	Street	
+		New	York,	NY	10001	
+		USA	*/
+		System.out.println("Alex Johnson");
+		System.out.println("23 Main Street");
+		System.out.println("New York, NY 10001");
+		System.out.println("USA");
+
+	}
+
+}
