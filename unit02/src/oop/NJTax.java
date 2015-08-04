5a2df5ff3fbc0010735306a68e607a5e98012530
Viktor Chmel <Viktor.Chmel@travisperkins.co.uk>
2015-08-05 02:09:30 +0300

    [add] NJ Tax rules

--- unit02/src/oop/NJTax.java
+++ unit02/src/oop/NJTax.java
@@ -1,5 +1,8 @@
 package oop;
 
-public class NJTax {
-
+public class NJTax extends Tax{
+	public double calcTax(){
+		if (grossIncome > 50000) return grossIncome*0.13;
+		else return grossIncome*0.1;
+	}
 }
--- unit02/src/oop/TestTax.java
+++ unit02/src/oop/TestTax.java
@@ -3,7 +3,7 @@ package oop;
 public class TestTax {
 
 	public static void main(String[] args) {
-		Tax t = new Tax();
+		Tax t = new NJTax();
 		t.dependents = 2;
 		t.grossIncome = 1000.0;
 		t.state = "NJ";
