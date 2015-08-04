8442f86c3deb9373d8b4d45d3259d6633bd39edb
Viktor Chmel <Viktor.Chmel@travisperkins.co.uk>
2015-08-04 12:32:31 +0300

    [add] HelloWorld example out of Eclipse scope

---
+++ unit01/HelloWorldOutOfEclipse.java
@@ -0,0 +1,11 @@
+public class HelloWorldOutOfEclipse{
+
+	public static void main(String[] args){
+		System.out.println("Hi!");
+	}
+}
+
+// javac HelloWorldOutOfEclipse.java
+// Run by:
+//  java -classpath ./ HelloWorldOutOfEclipse
+// Hi!
