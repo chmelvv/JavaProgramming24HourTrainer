ffab2a381a4527c7b09307106d4fb794a10a26b5
Viktor Chmel <Viktor.Chmel@travisperkins.co.uk>
2015-08-12 16:16:03 +0300

    [cnhg] reconfigure project

---
+++ unit03/src/CppPcc.java
@@ -0,0 +1,13 @@
+public class CppPcc {
+
+	public static void main(String[] args) {
+		int i = 1;
+		int j = i++;
+		System.out.println(j); //1
+		 j= ++i;
+		System.out.println(j); //3
+		
+
+	}
+
+}
---
+++ unit03/src/TestTax.java
@@ -0,0 +1,12 @@
+class TestTax{
+public static void main(String[] args){
+	double grossIncome = Double.parseDouble(args[0]);
+	String state = args[1];
+	int dependents = Integer.parseInt(args[2]);
+
+	Tax t = new Tax(grossIncome, state, dependents);
+	double yourTax = t.calcTax(); //calculating tax
+	//Printing the result
+	System.out.println("Your tax is " + yourTax);
+}
+}
