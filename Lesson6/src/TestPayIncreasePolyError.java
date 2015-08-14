23cc1467eaa970d528b8597a7036e6c571d7dd5c
Viktor Chmel <Viktor.Chmel@travisperkins.co.uk>
2015-08-14 03:04:29 +0300

    [add] error

--- Lesson6/src/TestPayIncreasePolyError.java
+++ Lesson6/src/TestPayIncreasePolyError.java
@@ -4,13 +4,13 @@ public class TestPayIncreasePolyError {
 
 	public static void main(String[] args) {
 
-        Person workers[] = new Person[3];
+        Payable workers[] = new Payable[3];
 		workers[0] = new Employee("John");
 		workers[1] = new Contractor("Mary");
 		workers[2] = new ForeignContractor("Boris");
 		
 		
-			for (Person p: workers){
+			for (Payable p: workers){
 			          ((Payable)p).increasePay(30);
 	 		}
 		} 
