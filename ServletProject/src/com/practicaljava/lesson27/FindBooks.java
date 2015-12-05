0818d034001fdba29305e4f42d71b4d7c3a5df69
Viktor Chmel <Viktor.Chmel@travisperkins.co.uk>
2015-12-06 01:02:04 +0200

    [done] servlet

---
+++ ServletProject/src/com/practicaljava/lesson27/FindBooks.java
@@ -0,0 +1,24 @@
+package com.practicaljava.lesson27;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by Viktor_Chmel on 12/6/2015.
+ */
+@WebServlet(urlPatterns="/books", name="FindBooks")
+public class FindBooks extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Hello, EE world!</h1>");
+    }
+}
